565e23ef8e32e4ce95e09842b6a7861427c5e713
Lin Ling Zheng <t108590056@ntut.org.tw>
2021-06-11 18:54:01 +0800

    x

--- FinalProject/app/src/main/java/com/matti/finalproject/MapsActivity.java
+++ FinalProject/app/src/main/java/com/matti/finalproject/MapsActivity.java
@@ -452,5 +452,38 @@ public class MapsActivity extends AppCompatActivity
         }
     }
     // [END maps_current_place_update_location_ui]
+
+    public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+
+        }
+
+        // Update the map configuration at runtime.
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            // Set the map coordinates to Kyoto Japan.
+            LatLng kyoto = new LatLng(35.00116, 135.7681);
+            // Set the map type to Hybrid.
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            // Add a marker on the map coordinates.
+            googleMap.addMarker(new MarkerOptions()
+                    .position(kyoto)
+                    .title("Kyoto"));
+            // Move the camera to the map coordinates and zoom in closer.
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(kyoto));
+            googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+            // Display traffic.
+            googleMap.setTrafficEnabled(true);
+
+        }
+    }
 }
 
