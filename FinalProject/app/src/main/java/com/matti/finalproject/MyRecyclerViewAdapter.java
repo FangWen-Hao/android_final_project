6e48cda2506a355f177dc6f18477dfdc1d955cff
Lin Ling Zheng <t108590056@ntut.org.tw>
2021-06-20 01:36:21 +0800

    x

--- FinalProject/app/src/main/java/com/matti/finalproject/MyRecyclerViewAdapter.java
+++ FinalProject/app/src/main/java/com/matti/finalproject/MyRecyclerViewAdapter.java
@@ -54,7 +54,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
 
         ViewHolder(View itemView) {
             super(itemView);
-            myTextView = itemView.findViewById(R.id.tvAnimalName);
+            myTextView = itemView.findViewById(R.id.Location1);
             itemView.setOnClickListener(this);
         }
 
